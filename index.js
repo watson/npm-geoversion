75d9cc6bd2f9af857a58018224dad3ff674e0e69
Thomas Watson Steen <w@tson.dk>
2015-08-14 17:12:32 +0200

    Use npm-version-bump module

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 #!/usr/bin/env node
 'use strict'
 
-var util = require('util')
-var exec = require('child_process').exec
-var git = require('git-state')
 var locate = require('wifi-triangulate')
 var pkgio = require('package-json-io')
-var bump = require('./lib/bump')
+var bump = require('npm-version-bump')
 var cliPkg = require('./package')
 
-var args = process.argv.slice(2)
-var cwd = process.cwd()
-var isGit = git.isGitSync(cwd)
+var command = process.argv.slice(2)[0]
 
-if (isGit) {
-  git.dirty(cwd, function (err, dirty) {
-    if (err) return done(err)
-    if (dirty > 0) return done(new Error('Git working directory not clean'))
-    updatePackage()
-  })
-} else {
-  updatePackage()
-}
+if (!command) usage()
+else bump(process.cwd(), command, precommit, done)
 
-function updatePackage () {
+function precommit (cb) {
   pkgio.read(function (err, pkg) {
     if (err) return done(err)
-
-    updateVersion(pkg)
 
     updateLocation(pkg, function (err) {
       if (err) {
@@ -39,28 +25,10 @@ function updatePackage () {
         return
       }
 
-      pkgio.update(pkg, function (err) {
-        if (err) return done(err)
-
-        if (isGit) {
-          commit(pkg.version, function (err) {
-            done(err, pkg)
-          })
-        } else {
-          done(null, pkg)
-        }
-      })
+      console.log('v%s @ %s/%s', pkg.version, pkg.coordinates[0], pkg.coordinates[1])
+      pkgio.update(pkg, cb)
     })
   })
-}
-
-function updateVersion (pkg) {
-  var version = bump(pkg, args[0])
-
-  if (!version) return usage()
-  if (pkg.version === version) return done(new Error('Version not changed'))
-
-  pkg.version = version
 }
 
 function updateLocation (pkg, cb) {
@@ -72,12 +40,8 @@ function updateLocation (pkg, cb) {
   })
 }
 
-function commit (version, cb) {
-  var cmd = util.format('git add package.json && git commit -m "%s" && git tag v%s', version, version)
-  exec(cmd, { cwd: cwd }, cb)
-}
-
 function usage () {
+  console.log('%s v%s', cliPkg.name, cliPkg.version)
   console.log('Usage:')
   console.log('  %s (<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease)', cliPkg.name)
   process.exit(1)
@@ -87,8 +51,5 @@ function done (err, pkg) {
   if (err) {
     console.error('ERROR:', err.message)
     process.exit(1)
-    return
   }
-  console.log('v%s @ %s/%s', pkg.version, pkg.coordinates[0], pkg.coordinates[1])
-  process.exit()
 }
